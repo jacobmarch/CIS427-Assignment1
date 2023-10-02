dfc6a22cd1eb4e1372c8d81b0efa6e7a06caf84a
Jacob Marchywka <dcfalcons2021@gmail.com>
2023-10-02 11:13:05 -0400

    Created basic client, need to do additional error handling on backend

--- client.py
+++ client.py
@@ -1,19 +1,42 @@
 import socket
-from command_handler import CommandHandler
 from constants import SERVER_PORT
 
 class Client:
 
-    def __innit__(self):
+    def __init__(self):  # Corrected typo from __innit__ to __init__
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def start(self):
         try:
-            self.client_socket.connect('0.0.0.0', SERVER_PORT)
+            self.client_socket.connect(('127.0.0.1', SERVER_PORT))
             print(f"Connection established with server via port {SERVER_PORT}.")
+            self.interactive_mode()
+        except Exception as e:
+            print(f"Error establishing connection with server. Error: {e}")
 
-        except:
-            print(f"Error establishing connection with server.")
+    def interactive_mode(self):
+        while True:
+            # 1. Prompt the user for input
+            command = input("Enter command (or 'exit' to disconnect): ")
 
-def shutdown(self):
-    self.client_socket.close()
+            # Exit condition for interactive mode
+            if command.lower() == 'exit':
+                print("Disconnecting from server...")
+                break
+
+            # 2. Send the command to the server
+            self.client_socket.sendall(command.encode('utf-8'))
+
+            # 3. Receive the response from the server
+            response = self.client_socket.recv(1024).decode('utf-8')
+
+            # 4. Display the server's response to the user
+            print("Server:", response)
+
+    def shutdown(self):
+        self.client_socket.close()
+
+if __name__ == "__main__":
+    client = Client()
+    client.start()
+    client.shutdown()
